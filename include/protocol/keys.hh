390f71cc406675ec13de24425fdf9f8c7fc9703c
vgatherps <vgatherps@gmail.com>
2023-02-12 22:46:02 +0800

    Start adding skeleton profile

---
+++ include/protocol/keys.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+// Stand-in for real symbology
+using NameKey = std::array<char, 8>;
+
+static_assert(sizeof(NameKey) == 8, "Bad NameKey size");
+
+// Some notes
+// It doesn't make sense to have specific receive-all
+// streams, since thay just forwards all messages in the
+// order they are received anyways
+//
+// Similarly, it doesn't make sense to have any generic most-recent
+// streams, since there's a fundamental assumption that out-of-order
+// behind messages can just be dropped
+enum class StreamType {
+  Ordered,
+  ReceiveAll,
+  MostRecent,
+  Depth,
+  BBO,
+};
+
+struct StreamKey {
+  NameKey exchange;
+  NameKey market;
+};

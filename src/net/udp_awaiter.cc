f053506d63a41668859d4b03ae316d74262a1041
vgatherps <vgatherps@gmail.com>
2023-02-14 12:23:52 +0800

    Simplify request chains with coroutines

--- src/net/udp_awaiter.cc
+++
@@ -1,5 +0,0 @@
-#include "net/udp_awaiter.hh"
-
-namespace blastoise::net {
-  UdpAwaiter::UdpAwaiter(seastar::pipe_reader<PacketBatch> p) : incoming_packet_batches(std::move(p)) {}
-}
--- src/net/udp_group.cc
+++ src/net/udp_group.cc
@@ -5,8 +5,7 @@
 
 namespace blastoise::net {
 
-
-  seastar::future<> UdpGroup::send_to_all(UdpPacket &packet) {
+seastar::future<> UdpGroup::send_to_all(UdpPacket &packet) {
   // as far as I can tell, you *do not* need to await a seastar future
   // for it to be run to completion? Hence just schedule and ignore
   // the UDP sends since they must be in order
@@ -16,7 +15,6 @@ namespace blastoise::net {
   // whether we have to block (and should throttle). However
   // real limits on small lines come far before machine limits
 
-
   co_await this->outstanding_batch_tracker.wait();
 
   for (auto &[id, socket] : sockets) {
@@ -24,29 +22,27 @@ namespace blastoise::net {
     cached_batch.push_back(std::move(sent));
   }
 
-  // this itself takes ownership of the futures. It's too bad it's hard to make an iterator yielding
-  // these futures, so there's a somewhat inefficient accumulation inside here
-  seastar::future<> await_all = seastar::when_all_succeed(cached_batch.begin(), cached_batch.end());
+  // this itself takes ownership of the futures. It's too bad it's hard to make
+  // an iterator yielding these futures, so there's a somewhat inefficient
+  // accumulation inside here
+  seastar::future<> await_all =
+      seastar::when_all_succeed(cached_batch.begin(), cached_batch.end());
 
   cached_batch.clear();
 
-  // TODO ownership problem here - if these futures try to resolve themselves during a shutdown
-  // and catch an exception, they'd reference garbage memory.
+  // TODO ownership problem here - if these futures try to resolve themselves
+  // during a shutdown and catch an exception, they'd reference garbage memory.
   // Need to put the semaphore into a shared ptr of sorts
   // or need to relearn enable_shared_from_this?
-  seastar::future<> breaks_semaphore = await_all.handle_exception([this] (auto exception) {
-    this->outstanding_batch_tracker.broken();
-    throw exception;
-  });
 
-  seastar::future<> signals_completion = breaks_semaphore.then([this]() {
-    this->outstanding_batch_tracker.signal(1);
-  });
-
-
-  co_await std::move(signals_completion);
-
-  co_return;
+  try {
+    co_await std::move(await_all);
+    outstanding_batch_tracker.signal(1);
+    co_return;
+  } catch (...) {
+    outstanding_batch_tracker.broken();
+    throw;
+  }
 }
 
 } // namespace blastoise::net
